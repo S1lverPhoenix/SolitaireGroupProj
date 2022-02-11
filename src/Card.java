c44bc554d850568fd401e1eb614fa61fbc80044e
Anika Parkhi <arparkhi@icloud.com>
2022-02-11 11:25:22 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -3,6 +3,16 @@ import java.awt.event.ActionEvent;
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
+    private int locX;
+    private int locY;
+    private String suit;
+    private boolean faceUp;
+    private int value;
+
+    public Card(String s, int v){
+        suit = s;
+        value = v;
+    }
 
     @Override
     public void update(ActionEvent a) {
